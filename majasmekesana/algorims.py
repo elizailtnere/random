1752f704d7fd4103cbace7d22b37717be92e7f51
elizailtnere <eliza.iltnere@gmail.com>
2025-02-04 12:01:34 +0200

    .

---
+++ majasmekesana/algorims.py
@@ -0,0 +1,59 @@
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from PIL import Image
+
+bildes = []
+label = []
+
+bilzu_adrese = "maju_meklesana/bildes/"
+
+modelis = RandomForestClassifier()
+
+def modela_trenesana(modelis):
+
+    for nosaukums in os.listdir(bilzu_adrese):
+        image = Image.open(os.path.join(bilzu_adrese,nosaukums)).resize((200,200),Image.Resampling.NEAREST)
+        bildes.append(np.array(image))
+    if "maja" in nosaukums:
+        label.append(1)
+    else:
+        label.append(0)
+
+
+
+bildes = np.array(bildes)
+label = np.array(label)
+
+bildes = bildes/255.0
+bildes = bildes.reshape(bildes.shape[0], -1)
+
+
+X_train, x_test, Y_train, y_test = train_test_split(bildes, label, test_size=0.2)
+
+modelis = RandomForestClassifier()
+
+modelis.fit(X_train, Y_train)
+
+rezultats = modelis.predict(x_test)
+
+precizitate = accuracy_score(rezultats, y_test)
+
+print(precizitate)
+
+def majas_noteiksana(bilde, modelis):
+    bilde = [Image.open(bilde).resize((200,200),Image.Resampling.NEAREST)]
+    bilde = np.array(bilde)
+    bilde = bilde/255.0
+    bilde = bildes.reshape(bildes.shape[0], -1)
+    rezultats = modelis.predict(bilde)
+    if rezultats[0] == 1:
+        print("Šajā bildē ir māja")
+    else:
+        print ("Nav māja")
+    return rezultats
+
+modelis = modela_trenesana(modelis)
+majas_noteiksana("majasmeklesana/kokamaja.jpg", modelis)
