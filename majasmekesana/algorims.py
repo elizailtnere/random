b2d8bc89b01d4ad2554197716e0b7ebb68e98104
elizailtnere <eliza.iltnere@gmail.com>
2025-02-25 11:31:42 +0200

    .

--- majasmekesana/algorims.py
+++ majasmekesana/algorims.py
@@ -5,49 +5,49 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 
-bildes = []
-label = []
 
-bilzu_adrese = "maju_meklesana/bildes/"
+bilzu_adrese = "majasmekesana/bildes/"
 
 modelis = RandomForestClassifier()
 
 def modela_trenesana(modelis):
+    bildes = []
+    label = []
 
     for nosaukums in os.listdir(bilzu_adrese):
         image = Image.open(os.path.join(bilzu_adrese,nosaukums)).resize((200,200),Image.Resampling.NEAREST)
         bildes.append(np.array(image))
-    if "maja" in nosaukums:
-        label.append(1)
-    else:
-        label.append(0)
+        if "maja" in nosaukums:
+            label.append(1)
+        else:
+            label.append(0)
 
 
 
-bildes = np.array(bildes)
-label = np.array(label)
+    bildes = np.array(bildes)
+    label = np.array(label)
 
-bildes = bildes/255.0
-bildes = bildes.reshape(bildes.shape[0], -1)
+    bildes = bildes/255.0
+    bildes = bildes.reshape(bildes.shape[0], -1)
 
 
-X_train, x_test, Y_train, y_test = train_test_split(bildes, label, test_size=0.2)
+    X_train, x_test, Y_train, y_test = train_test_split(bildes, label, test_size=0.2)
 
-modelis = RandomForestClassifier()
 
-modelis.fit(X_train, Y_train)
+    modelis.fit(X_train, Y_train)
 
-rezultats = modelis.predict(x_test)
+    rezultats = modelis.predict(x_test)
 
-precizitate = accuracy_score(rezultats, y_test)
+    precizitate = accuracy_score(rezultats, y_test)
 
-print(precizitate)
+    print(precizitate)
+    return modelis
 
 def majas_noteiksana(bilde, modelis):
     bilde = [Image.open(bilde).resize((200,200),Image.Resampling.NEAREST)]
     bilde = np.array(bilde)
     bilde = bilde/255.0
-    bilde = bildes.reshape(bildes.shape[0], -1)
+    bilde = bilde.reshape(bilde.shape[0], -1)
     rezultats = modelis.predict(bilde)
     if rezultats[0] == 1:
         print("Šajā bildē ir māja")
@@ -56,4 +56,4 @@ def majas_noteiksana(bilde, modelis):
     return rezultats
 
 modelis = modela_trenesana(modelis)
-majas_noteiksana("majasmeklesana/kokamaja.jpg", modelis)
+majas_noteiksana("majasmekesana/kokamaja.jpg", modelis)
